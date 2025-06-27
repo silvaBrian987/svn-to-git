fb230c9f85067eb8ec2fbc8949d97a1b401531da
Brian Silva <brian.silva@swissmedical.com.ar>
2025-06-27 02:02:55 +0000

    Update Python version to 3.12 in devcontainer and Pipfile; enhance migration script for SVN to Git

--- svn_to_git.py
+++ svn_to_git.py
@@ -2,13 +2,13 @@ from os import mkdir, rmdir
 import os
 import subprocess
 import svn.remote
-import shutil
 import platform
 import sys
 import psutil
+import git
 from git import Repo, RemoteReference
 import argparse
-import signal
+from csv import DictReader
 
 if platform.system() == "Windows":
     AUTHORS_DEFAULT_FILEPATH = "authors.txt"
@@ -16,6 +16,13 @@ else:
     AUTHORS_DEFAULT_FILEPATH = "./authors.txt"
 
 USER_HOME = os.path.expanduser("~")
+
+
+class RepoToMigrate:
+    def __init__(self, svn_url, git_path, svn_revisions=None):
+        self.svn_url = svn_url
+        self.git_path = git_path
+        self.svn_revisions = svn_revisions
 
 
 def kill_proc_tree(pid, including_parent=True):
@@ -36,7 +43,8 @@ def execute(cmd):
         # from msdn [1]
         CREATE_NEW_PROCESS_GROUP = 0x00000200  # note: could get it from subprocess
         DETACHED_PROCESS = 0x00000008  # 0x8 | 0x200 == 0x208
-        kwargs.update(creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
+        kwargs.update(creationflags=DETACHED_PROCESS |
+                      CREATE_NEW_PROCESS_GROUP)
     elif sys.version_info < (3, 2):  # assume posix
         kwargs.update(preexec_fn=os.setsid)
     else:  # Python 3.2+ and Unix
@@ -50,9 +58,10 @@ def execute(cmd):
         print("Waiting for process to finish...")
         for line in iter(process.stdout.readline, b""):
             aux = str(line.decode("utf-8").strip())
-            print(aux)
+            # print(aux)
             # print(f'type: {type(aux)}')
             output.append(aux)
+            print(aux)
         process.stdout.close()
         process.wait()
         return process.returncode, "\n".join(output)
@@ -61,7 +70,7 @@ def execute(cmd):
         kill_proc_tree(process.pid, including_parent=False)
         # exit(-1)
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
         kill_proc_tree(process.pid, including_parent=False)
         return process.returncode, "\n".join(output)
 
@@ -78,6 +87,117 @@ def handler(func, path, exc_info):
         raise
 
 
+def migrateRepo(repoToMigrate: RepoToMigrate, git_base_url, svn_username, svn_password, svn_authors_file, migrate_from_copy=False, ignore_history=False, no_stdlayout=False, skip_svn_clone=False):
+    svn_url = repoToMigrate.svn_url
+    svn_repo = svn.remote.RemoteClient(
+        svn_url, username=svn_username, password=svn_password
+    )
+
+    if not skip_svn_clone:
+        cmd = ["svn", "--non-interactive", "--username", svn_username,
+               "--password", svn_password, "info", svn_url]
+        rc, output = execute(cmd)
+        if rc != 0:
+            raise Exception(f"Error: {rc}\n{output}")
+
+    if not skip_svn_clone:
+        svn_revisions = repoToMigrate.svn_revisions
+        if not svn_revisions or (migrate_from_copy or ignore_history):
+            svn_info = svn_repo.info()
+            latest_commit = svn_info["commit_revision"]
+            print(f'Last commit: {latest_commit}')
+            svn_revisions = f"BASE:{latest_commit}"
+            if migrate_from_copy:
+                log = list(svn_repo.log_default(stop_on_copy=True))[-1]
+                svn_revisions = f"{log.revision}:{latest_commit}"
+            if ignore_history:
+                svn_revisions = str(int(latest_commit) - 1) + \
+                    ":" + str(int(latest_commit))
+    reponame = repoToMigrate.git_path
+    repodir = os.path.join(os.path.curdir, "git_repos/", reponame)
+
+    if not skip_svn_clone:
+        # shutil.rmtree(repodir, onerror = handler)
+        keep_goin = True
+        while keep_goin:
+            cmd = [
+                "git",
+                "svn",
+                "clone",
+                f"--username={svn_username}",
+                "--stdlayout" if not no_stdlayout else "",
+                f"--revision={svn_revisions}" if svn_revisions else "",
+                f"--authors-file={svn_authors_file}",
+                svn_url,
+                repodir,
+            ]
+            rc, output = execute(cmd)
+            print(f"rc: {rc}")
+            if rc == 0 or rc == 128:  # 128 means repository already exists
+                keep_goin = False
+            else:
+                raise Exception(f"Error: {rc}\n{output}")
+    repo = Repo(repodir)
+    # git_cmd = repo.git
+    # git_cmd.svn("clone",
+    #         f"--username={svn_username}",
+    #         "--stdlayout" if not no_stdlayout else "",
+    #         f"--revision={svn_revisions}" if svn_revisions else "",
+    #         f"--authors-file={svn_authors_file}",
+    #         svn_url,
+    #         repodir
+    #         )
+
+    for ref in repo.refs:
+        print(f"Processing ref: {ref.path}")
+        if "remotes/origin" in ref.path:
+            remote: RemoteReference = ref
+            print(f"Remote branch: {remote.path}")
+            remote_path = remote.path
+            path_split = remote.path.split("/")
+            branch_name = path_split[-1]
+            if path_split[-2] == "tags":
+                print(f"Tag: {branch_name}")
+                branch_name = "tags/" + branch_name
+                if branch_name in repo.tags:
+                    print(f"Tag already exists: {branch_name}")
+                    continue
+                repo.create_tag(
+                    branch_name, ref=ref, message="Tag created by svn_to_git.py"
+                )
+                continue
+
+            if branch_name in repo.refs:
+                print(
+                    f"WARN: Ignoring branch {branch_name} because already exists")
+                continue
+            if branch_name == "trunk":
+                print(f"WARN: Ignoring trunk because is already the branch master")
+                continue
+            # remote.rename(branch_name)
+            print(f"Creating branch {branch_name} from {remote_path}")
+            repo.create_head(branch_name, ref)
+    
+    if not "main" in repo.branches and "master" in repo.branches:
+        print(f"Creating branch main")
+        repo.branches.master.rename("main")
+
+    remote_url = git_base_url + reponame + ".git"
+    if not "origin" in repo.remotes:
+        print(f"Creating remote url {remote_url}")
+        repo.create_remote("origin", remote_url)
+    elif not git_base_url in repo.remotes.origin.url:
+        print(f"Updating remote url {remote_url}")
+        repo.remotes.origin.set_url(remote_url)
+
+    rc, output = execute(["git", "-C", repodir, "push", "origin", "--all"])
+    if rc != 0:
+        raise Exception(f"Error: {rc}\n{output}")
+    rc, output = execute(["git", "-C", repodir, "push", "origin", "--tags"])
+    if rc != 0:
+        raise Exception(f"Error: {rc}\n{output}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("SVN to Git Migration Tool")
     parser.add_argument("--svn-username", required=True)
@@ -86,10 +206,12 @@ if __name__ == "__main__":
     parser.add_argument("--svn-authors-file", default=AUTHORS_DEFAULT_FILEPATH)
     parser.add_argument("--svn-revisions", default="BASE:HEAD")
     parser.add_argument("--git-repos-path", default="git_repos/")
-    parser.add_argument("--git-base-url", default="https://github.com/username/")
+    parser.add_argument(
+        "--git-base-url", default="https://github.com/username/")
     parser.add_argument("--ignore-history", action="store_true")
     parser.add_argument("--no-stdlayout", action="store_true")
     parser.add_argument("--migrate-from-copy", action="store_true")
+    parser.add_argument("--skip-svn-clone", action="store_true")
 
     args = parser.parse_args()
 
@@ -100,97 +222,28 @@ if __name__ == "__main__":
     no_stdlayout = args.no_stdlayout
     ignore_history = args.ignore_history
     migrate_from_copy = args.migrate_from_copy
-
-    lines = []
-    with open(args.svn_repos_file, "r") as f:
-        lines = f.readlines()
+    svn_authors_file = args.svn_authors_file
+    skip_svn_clone = args.skip_svn_clone
 
     if not os.path.exists(args.git_repos_path):
         mkdir(args.git_repos_path)
 
-    for line in lines:
-        line = line.strip()
-        if line.startswith("#"):
-            continue
-        line = line.split(",")
-        if len(line) != 2:
-            continue
-        print(line)
-        r = svn.remote.RemoteClient(
-            line[0], username=svn_username, password=svn_password
-        )
-        
-        svn_info = r.info()
-        latest_commit = svn_info["commit_revision"]
-        print(f'Last commit: {latest_commit}')
-        if migrate_from_copy:
-            log = list(r.log_default(stop_on_copy=True))[-1]
-            svn_revisions = f"{log.revision}:{latest_commit}"
-        if ignore_history:
-            svn_revisions = str(int(latest_commit) - 1) + ":" + str(int(latest_commit))
-        reponame = line[1]
-        repodir = os.path.join(os.path.curdir, "git_repos/", reponame)
+    with open(args.svn_repos_file, "r") as f:
+        reader = DictReader(f)
 
-        # shutil.rmtree(repodir, onerror = handler)
-        cmd = [
-            "git",
-            "svn",
-            "clone",
-            f"--username={svn_username}",
-            "--stdlayout" if not no_stdlayout else "",
-            f"--revision={svn_revisions}" if svn_revisions else "",
-            f"--authors-file={args.svn_authors_file}",
-            line[0],
-            repodir,
-        ]
-        rc, output = execute(cmd)
-        if rc != 0:
-            print(f"Error: {rc}\n{output}")
-            continue
+        for row in reader:
+            repoToMigrate = RepoToMigrate(svn_url=row.get("svn_url"),
+                                          git_path=row.get("git_path"),
+                                          svn_revisions=row.get("svn_revisions", svn_revisions))
+            if repoToMigrate.svn_url.startswith("#"):
+                continue  # skip comments
 
-        repo = Repo(repodir)
-        for ref in repo.refs:
-            if "remotes/origin" in ref.path:
-                remote: RemoteReference = ref
-                print(f"Remote branch: {remote.path}")
-                remote_path = remote.path
-                path_split = remote.path.split("/")
-                branch_name = path_split[-1]
-                if path_split[-2] == "tags":
-                    print(f"Tag: {branch_name}")
-                    branch_name = "tags/" + branch_name
-                    if branch_name in repo.tags:
-                        print(f"Tag already exists: {branch_name}")
-                        continue
-                    repo.create_tag(
-                        branch_name, ref=ref, message="Tag created by svn_to_git.py"
-                    )
-                    continue
-                if branch_name in repo.refs:
-                    print(f"Ignoring branch {branch_name} because already exists")
-                    continue
-                if branch_name == "trunk":
-                    print(f"Ignoring trunk because is already the branch master")
-                    continue
-                # remote.rename(branch_name)
-                repo.create_head(branch_name, ref)
-
-        if not "main" in repo.branches and "master" in repo.branches:
-            print(f"Creating branch main")
-            repo.branches.master.rename("main")
-
-        if not "origin" in repo.remotes:
-            print(f"Creating remote url")
-            repo.create_remote("origin", remote_url_base + reponame + ".git")
-        elif not remote_url_base in repo.remotes.origin.url:
-            print(f"Updating remote url")
-            repo.remotes.origin.set_url(remote_url_base + reponame + ".git")
-
-        rc, output = execute(["git", "-C", repodir, "push", "origin", "--all"])
-        if rc != 0:
-            print(f"Error: {rc}\n{output}")
-            continue
-        rc, output = execute(["git", "-C", repodir, "push", "origin", "--tags"])
-        if rc != 0:
-            print(f"Error: {rc}\n{output}")
-            continue
+            try:
+                print(
+                    f"Migrating {repoToMigrate.svn_url} to {repoToMigrate.git_path}")
+                migrateRepo(repoToMigrate, git_base_url=remote_url_base, svn_username=svn_username, svn_password=svn_password,
+                            svn_authors_file=svn_authors_file, migrate_from_copy=migrate_from_copy, ignore_history=ignore_history, no_stdlayout=no_stdlayout, skip_svn_clone=skip_svn_clone)
+                print(f"Migration of {repoToMigrate.svn_url} completed.")
+            except Exception as e:
+                print(f"Error on migration of {repoToMigrate.svn_url}: {e}")
+                continue
